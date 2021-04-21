15fd6145088ef46888cc11ed189197a53bf21688
Nickolay Kholodnyack <nick@MacBook-Pro-Nickolay.local>
2021-04-21 14:23:46 +0300

    adding pre header

---
+++ src/stories/components/Header/Header.stories.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Header from './Header';
+
+export default {
+  title: 'Header/Header1',
+  component: Header,
+  argTypes: {
+    backgroundColor: {control: 'color'},
+  },
+};
+
+const Template = (args) => <Header {...args} />
+
+export const Header1 = Template.bind({});
+Header1.args = {
+  cls: "menu",
+  fontWeight: 400,
+  backgroundColor:"#fff",
+  fontSize: "16px",
+  fontFamily: "Asap",
+}
