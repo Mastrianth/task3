2dee2f26f1ed5d89daa2fdd61703c89932f51401
Nickolay Kholodnyack <nick@MacBook-Pro-Nickolay.local>
2021-05-05 21:34:30 +0300

    add reactLazy and dynamic imports

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import dynamic from 'next/dynamic';
+import LazyLoad from 'react-lazyload';
 import Banner from '../src/components/Banner';
-import Relationships from '../src/components/Relationship';
-import Users from '../src/components/Users';
 
-const AboutMe = dynamic(() => import('../src/components/Acquainted/Acquainted'));
+const AboutMe = dynamic(() => import('../src/components/Acquainted/Acquainted'),
+  { ssr: false });
+const Relationships = dynamic(() => import('../src/components/Relationship'),
+  { ssr: false });
+const Users = dynamic(() => import('../src/components/Users'),
+  { ssr: false });
 
 function Index() {
   return (
@@ -12,8 +16,12 @@ function Index() {
     <>
       <Banner />
       <AboutMe />
-      <Relationships />
-      <Users />
+      <LazyLoad once>
+        <Relationships />
+      </LazyLoad>
+      <LazyLoad once>
+        <Users />
+      </LazyLoad>
     </>
   );
 }
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,19 +1,22 @@
 import PropTypes from 'prop-types';
 import React, {
-  memo, useEffect, useState, useContext,
 } from 'react';
 import dynamic from 'next/dynamic';
+import LazyLoad from 'react-lazyload';
 import Header from '../Header';
 import classes from './Layout.module.scss';
 
-const Footer = dynamic(() => import('../Footer'));
-const Layout = ({ children, isPageSpeedNotFound }) => (
+const Footer = dynamic(() => import('../Footer'),
+  { ssr: false });
+const Layout = ({ children }) => (
   // eslint-disable-next-line react/jsx-filename-extension
   <>
     <div className={classes.siteWrapper}>
       <Header userName="Maximilian" userEmail="MaximilianMaximilian@gmail.com" logOut={() => { console.log('Log out'); }} />
       {children}
-      <Footer />
+      <LazyLoad once>
+        <Footer />
+      </LazyLoad>
     </div>
   </>
 );
