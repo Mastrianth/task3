a7030cd6947dcb1d5d70424b94691d68bb03f04c
Nickolay Kholodnyack <nick@MacBook-Pro-Nickolay.local>
2021-05-05 15:10:37 +0300

    back broken DS

--- next.config.js
+++ next.config.js
@@ -13,7 +13,8 @@ module.exports = withPlugins([
   [
     optimizedImages, {
       optimizeImages: false,
-      imagesPublicPath: isProd ? 'https://test2021-frontend-nick-k-task3-static.abzdev2.com/_next/static/images/' : '/_next/static/images/',
+      imagesPublicPath: isProd ? '/_next/static/images/' : '/_next/static/images/',
+      // imagesPublicPath: isProd ? 'https://test2021-frontend-nick-k-task3-static.abzdev2.com/_next/static/images/' : '/_next/static/images/',
       inlineImageLimit: 1,
     },
   ],
