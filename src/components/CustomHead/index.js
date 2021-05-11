5d749b24edb35081f546cc9c66beba252b075c96
Nickolay Kholodnyack <nick@MacBook-Pro-Nickolay.local>
2021-05-11 14:23:04 +0300

    add correct preload

--- src/components/CustomHead/index.js
+++ src/components/CustomHead/index.js
@@ -46,7 +46,7 @@ const CustomHead = ({ title, t, currentRoute, }) => {
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preload" as="image" href="https://test2021-frontend-nick-k-task3-static.abzdev2.com/_next/static/images/banner-background-360-2x-1cd3a846fcb64971da37100438adff5b.webp" />
-      <link rel="preload" as="image" href="https://test2021-frontend-nick-k-task3-static.abzdev2.com/_next/static/images/banner-reg-360-2x-2b8668deae9690115bd6cd3c8b656913.webp" />
+      <link rel="preload" as="image" href="https://test2021-frontend-nick-k-task3-static.abzdev2.com/_next/static/images/banner-reg-768-50d406430150a82b87ee895fc7c9753b.webp" />
       <meta name="robots" content="noindex, nofollow" />
       <script dangerouslySetInnerHTML={{
         __html: `
