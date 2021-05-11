9037aa7f344a7aa8e20fccdd1a7d26c6ccf48260
Nickolay Kholodnyack <nick@MacBook-Pro-Nickolay.local>
2021-05-11 14:16:10 +0300

    adding webp banner for reg banner

--- src/components/CustomHead/index.js
+++ src/components/CustomHead/index.js
@@ -2,10 +2,37 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Head from 'next/head';
 import favIcon from '../../../public/assets/favicon.png';
+import { INDEX, SIGN_UP, TERMS } from '../../routing/routes';
+import { withTranslation } from '../../../i18n';
 
-const CustomHead = ({ title }) => {
+const CustomHead = ({ title, t, currentRoute, }) => {
   const titleBase = 'Test assignment for Frontend Developer position';
   const displayTitle = title ? `${title} | ${titleBase}` : titleBase;
+
+  // let bannerBgPreload;
+  // let ogDescription;
+  // let ogImage;
+  // switch (currentRoute) {
+  //   case INDEX:
+  //     bannerBgPreload = <link rel="preload" href={mainBg768Webp} as="image" />;
+  //     ogDescription = t('We kindly remind you that your test assignment');
+  //     ogImage = 'https://task3-1-test2020-hryhorii-k.abzdev2.com/main.jpg';
+  //     break;
+  //   case SIGN_UP:
+  //     bannerBgPreload = <link rel="preload" href={registerBg768Webp} as="image" />;
+  //     ogDescription = t('Users want to find answers');
+  //     ogImage = 'https://task3-1-test2020-hryhorii-k.abzdev2.com/register.jpg';
+  //     break;
+  //   case TERMS:
+  //     ogDescription = t('A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring');
+  //     ogImage = 'https://task3-1-test2020-hryhorii-k.abzdev2.com/main.jpg';
+  //     break;
+  //   default:
+  //     bannerBgPreload = null;
+  //     ogDescription = t('We kindly remind you that your test assignment');
+  //     ogImage = 'https://task3-1-test2020-hryhorii-k.abzdev2.com/main.jpg';
+  // }
+
 
   return (
   // eslint-disable-next-line react/jsx-filename-extension
@@ -15,11 +42,11 @@ const CustomHead = ({ title }) => {
         name="description"
         content="We kindly remind you that your test assignment should be submitted as a link to github/bitbucket repository. Please be patient, we consider and respond to every application that meets minimum requirements. We look forward to your submission. Good luck!"
       />
-      <link rel="icon" type="image/svg+xml" href="%PUBLIC_URL%/favicon.svg" />
-      <link rel="icon" type="image/png" href="%PUBLIC_URL%/favicon.png" />
+      <link rel="icon" href={favIcon} />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preload" as="image" href="https://test2021-frontend-nick-k-task3-static.abzdev2.com/_next/static/images/banner-background-360-2x-1cd3a846fcb64971da37100438adff5b.webp" />
+      <link rel="preload" as="image" href="https://test2021-frontend-nick-k-task3-static.abzdev2.com/_next/static/images/banner-reg-360-2x-2b8668deae9690115bd6cd3c8b656913.webp" />
       <meta name="robots" content="noindex, nofollow" />
       <script dangerouslySetInnerHTML={{
         __html: `
