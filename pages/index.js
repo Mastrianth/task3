4f9d9fe682b319ded6be93208b7e2bd311141316
Nickolay Kholodnyack <nick@MacBook-Pro-Nickolay.local>
2021-05-18 11:33:02 +0300

    fix: removed dinamyc import

--- pages/index.js
+++ pages/index.js
@@ -4,13 +4,14 @@ import LazyLoad from 'react-lazyload';
 import Banner from '../src/components/Banner';
 import Relationships from '../src/components/Relationship';
 import Acquainted from '../src/components/Acquainted/Acquainted';
+import Users from '../src/components/Users';
 
 // const AboutMe = dynamic(() => import('../src/components/Acquainted/Acquainted'),
 //   { ssr: false });
 // const Relationships = dynamic(() => import('../src/components/Relationship'),
 //   { ssr: false });
-const Users = dynamic(() => import('../src/components/Users'),
-  { ssr: false });
+// const Users = dynamic(() => import('../src/components/Users'),
+//   { ssr: false });
 
 function Index() {
   return (
