50fe93eae53033f336d800fdbea96601ff9d1a89
Nickolay Kholodnyack <nick@MacBook-Pro-Nickolay.local>
2021-05-26 08:16:19 +0300

    feet: add preload img for main

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import Banner from '../src/components/Banner';
 import Relationships from '../src/components/Relationship';
 import Acquainted from '../src/components/Acquainted/Acquainted';
 import Users from '../src/components/Users';
-import socialImg from '../src/assets/img/banner/banner-background-360.webp';
+import mainBg360Webp from '../src/assets/img/banner/banner-background-360-2x.webp';
 
 function Index() {
   return (
@@ -14,19 +14,19 @@ function Index() {
         <title>HomePage - Task 3.1 Frontend Trainee Program</title>
         <meta name="title" content="HomePage - Task 3.1 Frontend Trainee Program" />
         <meta name="description" content="Home page - Test task 3 description for crawlers and scrapers" />
-        <link rel="preload" as="image" href="https://test2021-frontend-nick-k-task3-static.abzdev2.com/_next/static/images/banner-background-360-2x-1cd3a846fcb64971da37100438adff5b.webp" />
+        <link rel="preload" as="image" href={mainBg360Webp} />
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://test2021-frontend-nick-k-task3.abzdev2.com/" />
         <meta property="og:title" content="HomePage - Task 3.1 Frontend Trainee Program" />
         <meta property="og:description" content="Home page - Test task 3 description for crawlers and scrapers" />
-        <meta property="og:image" content={`https://test2021-frontend-nick-k-task3-static.abzdev2.com/${socialImg}`} />
+        <meta property="og:image" content="https://test2021-frontend-nick-k-task3.abzdev2.com/banner-background-768.jpg" />
         <meta property="og:image:width" content="1150" />
         <meta property="og:image:height" content="760" />
         <meta property="twitter:card" content="summary_large_image" />
         <meta property="twitter:url" content="https://test2021-frontend-nick-k-task3.abzdev2.com/" />
         <meta property="twitter:title" content="HomePage - Task 3.1 Frontend Trainee Program" />
         <meta property="twitter:description" content="Home page - Test task 3 description for crawlers and scrapers" />
-        <meta property="twitter:image" content={`https://test2021-frontend-nick-k-task3-static.abzdev2.com/${socialImg}`} />
+        <meta property="twitter:image" content="https://test2021-frontend-nick-k-task3.abzdev2.com/banner-reg-768.jpg" />
       </Head>
       <div>
         <Banner />
